8433c9f188fcae424902ce76f15af41c59acb3e3
Renan Guerra <rebarbosa07@hotmail.com>
2021-03-13 09:46:31 -0300

    smooth scroll

--- globalStyles.js
+++ globalStyles.js
@@ -5,6 +5,9 @@ const GlobalStyles = createGlobalStyle`
         margin: 0px;
         padding: 0px;
     }
+    html{
+        scroll-behavior: smooth;
+    }
     body {
         background-color: #F9F9FB;
         font-family:'Roboto',sans-serif;
